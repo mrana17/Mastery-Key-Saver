a4203f4ac98218f016ff64b18a9ac334a317a1a3
mrana17 <rm_a@msn.com>
2021-03-02 17:29:24 +0100

    ⚡️ connected to database MongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,49 @@
 import { handleGetPassword, handleSetPassword, hasAccess } from "./command";
 import { printNoAccess, printWelcomeMessage } from "./messages";
 import { askForAction, askForCredentials } from "./questions";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const run = async () => {
-  printWelcomeMessage();
-  const credentials = await askForCredentials();
-  if (!hasAccess(credentials.masterPassword)) {
-    printNoAccess();
-    run();
-    return;
+  const url = process.env.MONGODB_URL;
+
+  try {
+    const client = await MongoClient.connect(url, {
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to DB!");
+
+    const db = client.db("Mastery-Key-Saver-Mudi");
+
+    await db.collection("inventory").insertOne({
+      item: "schlüssel",
+      quantity: "17",
+      tags: ["codes"],
+      size: { h: 1, w: 2 },
+    });
+
+    client.close();
+  } catch (error) {
+    console.error(error);
   }
-  const action = await askForAction();
-  switch (action.command) {
-    case "set":
-      handleSetPassword(action.passwordName);
-      break;
-    case "get":
-      handleGetPassword(action.passwordName);
-      break;
-  }
+
+  // printWelcomeMessage();
+  // const credentials = await askForCredentials();
+  // if (!hasAccess(credentials.masterPassword)) {
+  //   printNoAccess();
+  //   run();
+  //   return;
+  // }
+  // const action = await askForAction();
+  // switch (action.command) {
+  //   case "set":
+  //     handleSetPassword(action.passwordName);
+  //     break;
+  //   case "get":
+  //     handleGetPassword(action.passwordName);
+  //     break;
+  // }
 };
 
 run();
