d63e8725c10843ad78bfbf97cc1e0857c5c99534
mrana17 <rm_a@msn.com>
2021-03-04 17:33:22 +0100

    ⚡️ include read, create to handle get, set

--- src/command.ts
+++ src/command.ts
@@ -1,18 +1,31 @@
 import { printPassword, printPasswordSet } from "./messages";
-import { askForPasswordValue } from "./questions";
+import { askForPasswordValue, askForPasswordName } from "./questions";
+import dotenv from "dotenv";
+import { closeDB, connectDB, createPasswordDoc, readPasswordDoc } from "./db";
+dotenv.config();
 
 export const hasAccess = (masterPassword: string): boolean =>
-  masterPassword === "abc123";
+  masterPassword === process.env.CRYPTO_MASTER_PASSWORD;
 
 export const handleSetPassword = async (
   passwordName: string
 ): Promise<void> => {
+  const nameForPassword = await askForPasswordName();
   const passwordValue = await askForPasswordValue();
+  await createPasswordDoc({
+    name: nameForPassword,
+    value: passwordValue,
+  });
   printPasswordSet(passwordName);
 };
 
 export const handleGetPassword = async (
   passwordName: string
 ): Promise<void> => {
-  printPassword(passwordName, "XYZ123");
+  const passwordDoc = await readPasswordDoc(passwordName);
+  if (!passwordDoc) {
+    console.log("You are not the Master!");
+    return;
+  }
+  printPassword(passwordDoc.name, passwordDoc.value);
 };
